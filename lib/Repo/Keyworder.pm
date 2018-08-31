c9d379f6b2a9e24b908f6bdbd23b799aa7a89a84
Kent Fredric <kentnl@gentoo.org>
2018-09-01 11:55:45 +1200

    Add package-level best-keyword extraction

--- lib/Repo/Keyworder.pm
+++ lib/Repo/Keyworder.pm
@@ -1,4 +1,4 @@
-use 5.006;  # our
+use 5.006;    # our
 use strict;
 use warnings;
 
@@ -13,21 +13,24 @@ our $VERSION = '0.001000';
 use Repo::Keyworder::Cache;
 
 sub new {
-  my ( $self, $repo ) = @_;
+  my ( $self, %opts ) = @_;
+
   return bless {
-    repo => $repo,
+    repo  => $opts{repo},
     cache => Repo::Keyworder::Cache->new(
-      repo => $repo,
+      repo => $opts{repo},
+      ( exists $opts{cache_fallbacks} ? ( cache_fallbacks => $opts{cache_fallbacks} ) : () ),
     ),
   }, $self;
 }
-use Data::Dump qw(pp);
+
 sub get_ebuild_keywords {
   my ( $self, $ebuild_rel ) = @_;
   my $path = $self->{repo} . '/' . $ebuild_rel;
   if ( !-e $path or -d $path ) {
     die "$path is not a file";
   }
+
   # Convert path to atom
   my ( $cat, $pkg, $version ) = $ebuild_rel =~ qr{
     ^
@@ -36,12 +39,62 @@ sub get_ebuild_keywords {
       \2-(.*?)\.ebuild
     $
   }x;
-  return $self->{cache}->get_keywords($cat,$pkg, $version);
+  return $self->{cache}->get_keywords( $cat, $pkg, $version );
 }
 
-
-
-
+sub get_package_keywords {
+  my ( $self, $catpn ) = @_;
+  my $path = $self->{repo} . '/' . $catpn;
+  if ( !-e $path or !-d $path ) {
+    die "$path is not a package";
+  }
+  my ( $cat, $pkg ) = $catpn =~ qr{
+  ^
+    ([^/]+) /
+    ([^/]+)
+  $
+  }x;
+  local ( $!, $? );
+  opendir my $pkdir, $path or die "can't opendir $path, $! $?";
+  my %keywords;
+  while ( my $ebuild = readdir $pkdir ) {
+    next unless $ebuild =~ /\.ebuild$/;
+    next if -d "${path}/${ebuild}";
+    my ($version) = $ebuild =~ qr{
+    ^
+      \Q${pkg}\E-
+      (.+)
+      \.ebuild
+     $
+    }x;
+    for my $keyword ( $self->{cache}->get_keywords( $cat, $pkg, $version ) ) {
+      if ( $keyword =~ /^~(.*)$/ ) {
+        if ( not exists $keywords{$1} ) {
+          $keywords{$1} = 'unstable';
+          next;
+        }
+        next;
+      }
+      if ( $keyword =~ /^([^~-].*)$/ ) {
+        if ( not exists $keywords{$1} ) {
+          $keywords{$1} = 'stable';
+          next;
+        }
+        next if $keywords{$1} eq 'stable';
+        if ( $keywords{$1} eq 'unstable' ) {
+          $keywords{$1} = 'stable';
+          next;
+        }
+      }
+      if ( $keyword =~ /^-(.*)$/ ) {
+        $keywords{$1} = 'blocked';
+        next;
+      }
+      warn "Unhandled keyword $keyword for $catpn version $version";
+    }
+  }
+  return \%keywords;
+}
 
 1;
 
--- t/basic.t
+++ t/basic.t
@@ -8,33 +8,57 @@ use Repo::Keyworder;
 use Data::Dump qw(pp);
 
 my $repo = "/usr/local/gentoo";
-my $r    = Repo::Keyworder->new($repo);
+my $r    = Repo::Keyworder->new(
+  repo            => $repo,
+  cache_fallbacks => ["/usr/portage/metadata/md5-cache/"],
+);
 
-scan_package('dev-lang', 'perl');
-scan_category_filter("virtual", sub { $_[0] =~ /^perl-/ });
+#scan_repo();
+scan_package_terse( 'dev-lang', 'perl' );
+scan_category_filter( "virtual", sub { $_[0] =~ /^perl-/ } );
 scan_category('perl-core');
 scan_category('dev-perl');
 
+sub scan_repo {
+  opendir my $repdir, "${repo}";
+  while ( my $category = readdir $repdir ) {
+    next if $category =~ /^..?$/;
+    next if $category =~ /^(metadata|profiles|\.git)$/;
+    next unless -d "${repo}/${category}";
+    scan_category($category);
+  }
+}
 sub scan_category {
   my ($category) = @_;
   opendir my $devperl, "${repo}/${category}";
   while ( my $package = readdir $devperl ) {
     next if $package =~ /^..?$/;
     next unless -d "${repo}/${category}/${package}";
-    scan_package( $category, $package );
-  }
-}
-sub scan_category_filter {
-  my ($category, $filter) = @_;
-  opendir my $devperl, "${repo}/${category}";
-  while ( my $package = readdir $devperl ) {
-    next if $package =~ /^..?$/;
-    next unless $filter->( $package );
-    next unless -d "${repo}/${category}/${package}";
-    scan_package( $category, $package );
+    scan_package_terse( $category, $package );
   }
 }
 
+sub scan_category_filter {
+  my ( $category, $filter ) = @_;
+  opendir my $devperl, "${repo}/${category}";
+  while ( my $package = readdir $devperl ) {
+    next if $package =~ /^..?$/;
+    next unless $filter->($package);
+    next unless -d "${repo}/${category}/${package}";
+    scan_package_terse( $category, $package );
+  }
+}
+
+sub scan_package_terse {
+  my ( $category, $package ) = @_;
+  my $keywords = $r->get_package_keywords( "${category}/${package}" );
+  printf "%s/%s: %s\n", $category, $package, join q[ ], map {
+    $keywords->{$_} eq 'unstable' ? "~$_"
+    : $keywords->{$_} eq 'stable' ? "$_"
+    : $keywords->{$_} eq 'blocked' ? "-$_" :
+    "($_)"
+  } sort keys %{$keywords};
+}
 sub scan_package {
   my ( $category, $package ) = @_;
   opendir my $packagedir, "${repo}/${category}/${package}";
