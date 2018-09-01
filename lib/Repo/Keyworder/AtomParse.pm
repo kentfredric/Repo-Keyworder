96f0e12c7c219b7164162224b5ef3f027e6ae9bc
Kent Fredric <kentnl@gentoo.org>
2018-09-02 11:11:36 +1200

    Add Cat/PN/Version atom splitting wrapper

--- lib/Repo/Keyworder/AtomParse.pm
+++ lib/Repo/Keyworder/AtomParse.pm
@@ -32,5 +32,24 @@ sub to_catpn {
   return $lines[0];
 }
 
+sub split_atom {
+  my ( $self, $depspec ) = @_;
+  if ( exists $self->{split_atom_cache}->{$depspec} ) {
+    return @{ $self->{split_atom_cache}->{$depspec} };
+  }
+  $depspec =~ s/\[.*\]//g;
+  local ( $!, $? );
+  open my $fh, '-|', 'qatom', '-F', '%{CATEGORY} %{PN} %{PV} %[PR]', $depspec or die "Can't invoke qatom, $! $?";
+  my (@lines) = <$fh>;
+  close $fh or warn "error closing qatom, $! $?";
+  chomp $lines[0];
+
+  return if not length $lines[0];
+  my ( $category, $pn, $pv, $pr ) = split / /, $lines[0];
+  $pv .= "-" . $pr if defined $pr and length $pr;
+  $self->{split_atom_cache}->{$depspec} = [ $category, $pn, $pv ];
+  return ( $category, $pn, $pv );
+}
+
 1;
 
